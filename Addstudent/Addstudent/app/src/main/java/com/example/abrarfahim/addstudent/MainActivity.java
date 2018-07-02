573ec62a67ee10c2280e111f4d4e34421552c234
Suria Sultana Lima <39965742+SuriaLima@users.noreply.github.com>
2018-07-02 20:04:18 +0600

    Add files via upload

---
+++ Addstudent/Addstudent/app/src/main/java/com/example/abrarfahim/addstudent/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.abrarfahim.addstudent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+private void onClickBackButton(View v) {
+//     finish();    // code for back button
+}
+private void onClickAddButton(View view) {
+    // code for add student button
+//    Intent intent=new Intent(this,MainActivity.class);
+//    startActivity(intent);
+}
+}
